06c239dff4a9d798a9d91664a258698ff96c4495
awadheshkumaryadav984-ux <awadheshkumaryadav984@gmail.com>
2026-07-10 12:12:30 +0530

    app.py

--- app.py
+++ app.py
@@ -119,3 +119,8 @@ Return ONLY JSON.
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
