bdfdbcc64420ed2b3d1168a2d53f6d4b57d5cb74
awadheshkumaryadav984-ux <awadheshkumaryadav984@gmail.com>
2026-07-11 17:45:21 +0530

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from google import genai
 from google.genai import types
-import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 CORS(app)
 
-# Replace with your NEW Gemini API key
-API_KEY = "AQ.Ab8RN6IYu4nRikfnQdhBhKHRbWDGxk1faFk9vRiEYhtPo2llmg"
+API_KEY = os.getenv("AQ.Ab8RN6IYu4nRikfnQdhBhKHRbWDGxk1faFk9vRiEYhtPo2llmg")
 
 client = genai.Client(api_key=API_KEY)
 
-@app.route("/")
-def home():
-    return "AI Disaster Management Backend Running"
-    from flask import render_template
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/analyze", methods=["POST"])
 def analyze():
 
     if "image" not in request.files:
-        return jsonify({"success": False, "message": "No image uploaded"})
+        return jsonify({
+            "success": False,
+            "message": "No image uploaded"
+        })
 
     image = request.files["image"]
     image_bytes = image.read()
@@ -42,34 +41,16 @@ Return ONLY valid JSON in this exact format:
   "severity":"",
   "confidence":"",
   "summary":"",
-  "immediate_actions":[
-    "",
-    "",
-    "",
-    "",
-    ""
-  ],
-  "safety_tips":[
-    "",
-    "",
-    "",
-    "",
-    ""
-  ],
+  "immediate_actions":["","","","",""],
+  "safety_tips":["","","","",""],
   "recommended_contacts":[
     "Police",
     "Fire Department",
     "Ambulance",
     "Disaster Management Authority"
   ],
-  "equipment_needed":[
-    "",
-    "",
-    ""
-  ]
+  "equipment_needed":["","",""]
 }
-
-Rules:
 
 Disaster Type:
 Flood
@@ -97,18 +78,18 @@ Return ONLY JSON.
     try:
 
         response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=[
-        prompt,
-        types.Part.from_bytes(
-            data=image_bytes,
-            mime_type=image.mimetype,
-        ),
-    ],
-    config=types.GenerateContentConfig(
-        response_mime_type="application/json"
-    )
-)
+            model="gemini-2.5-flash",
+            contents=[
+                prompt,
+                types.Part.from_bytes(
+                    data=image_bytes,
+                    mime_type=image.mimetype
+                )
+            ],
+            config=types.GenerateContentConfig(
+                response_mime_type="application/json"
+            )
+        )
 
         return jsonify({
             "success": True,
@@ -121,6 +102,7 @@ Return ONLY JSON.
             "success": False,
             "message": str(e)
         })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
